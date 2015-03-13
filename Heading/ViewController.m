cb730abf3b143787cb2c5401a3d13647659e84a3
Chen <Chen@Chens-MBP.dhcp.pvgl.sap.corp>
2015-03-13 17:35:54 +0800

    demo

--- Heading/ViewController.m
+++ Heading/ViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
-
+@interface ViewController () <CLLocationManagerDelegate>
+@property (nonatomic , strong) CLLocationManager *locationManager;
+@property (nonatomic , strong) NSOperationQueue *queue;
+@property (nonatomic , strong) CALayer *naviIcon;
 @end
 
 @implementation ViewController
@@ -17,6 +20,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    if ([CLLocationManager headingAvailable]) {
+        
+        self.locationManager = [[CLLocationManager alloc] init];
+        self.locationManager.delegate = self;
+        [self.locationManager startUpdatingHeading];
+    }
+    else{
+        [self.headingLabel setText:@"此设备没有磁力计"];
+    }
+    
+    self.naviIcon = [[CALayer alloc] init];
+    self.naviIcon.frame = CGRectMake(170,230,30,30);
+    self.naviIcon.contents = (id)[[UIImage imageNamed:@"location.png"] CGImage];
+    [self.view.layer addSublayer:self.naviIcon];
+}
+
+-(void) locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading{
+    
+    CGFloat headings = M_PI*newHeading.trueHeading/180.0f;
+    //CGFloat headings = newHeading.trueHeading;
+    CABasicAnimation* anim = [CABasicAnimation animationWithKeyPath:@"transform"];
+    
+    CATransform3D fromValue = self.naviIcon.transform;
+    anim.fromValue = [NSValue valueWithCATransform3D:fromValue];
+    
+    CATransform3D toValue = CATransform3DMakeRotation(headings, 0, 0, 1);
+    anim.toValue = [NSValue valueWithCATransform3D:toValue];
+    anim.duration = 0.2;
+    anim.removedOnCompletion = YES;
+    self.naviIcon.transform = toValue;
+    [self.naviIcon addAnimation:anim forKey:nil];
+    
+    [self.headingLabel setText:[NSString stringWithFormat:@"与北方的方向:\n%f", newHeading.trueHeading]];
 }
 
 - (void)didReceiveMemoryWarning {
