781730bf5d45b4e2875db225c66957eedef3f7ad
Zion <751735080@qq.com>
2015-03-16 14:57:48 +0800

    test

---
+++ Heading WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  Heading WatchKit Extension
+//
+//  Created by Chen on 15/3/15.
+//  Copyright (c) 2015年 Chen. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
